cab7b5b417ee56a1117d16bcd4a866a4fea99d15
Daniel Grzenda <dcgrzenda@gmail.com>
2018-04-11 11:39:47 -0700

    added some views and paths

--- sudofood/login/views.py
+++ sudofood/login/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import User
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World! You are at the login index!")
+    template = loader.get_template('login/index.html')
+    context = {}
+    return render(request, 'login/index.html', context) 
+    # return HttpResponse(template.render(context,request))
+
+def user(request, user_id):
+    return HttpResponse("This is the profile page for user %s!"%(user_id))
+
 
