7b4aa8a819b1f3e9661ab58039d417d2d3812700
danielleasavage <dsavage@dons.usfca.edu>
2018-04-12 16:29:53 -0700

    PEP8

--- sudofood/login/models.py
+++ sudofood/login/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class User(models.Model):
@@ -9,25 +10,24 @@ class User(models.Model):
     city = models.CharField(max_length=100)
     weight = models.PositiveSmallIntegerField()
     height = models.PositiveSmallIntegerField()
-    
+   
     LOSE_WEIGHT = 'LW'
     MAINTAIN_WEIGHT = 'MW'
     GAIN_WEIGHT = 'GW'
     GOALS = (
-        (LOSE_WEIGHT,'Lose Weight'),
-        (MAINTAIN_WEIGHT,'Maintain Weight'),
-        (GAIN_WEIGHT,'Gain Weight'),
+        (LOSE_WEIGHT, 'Lose Weight'),
+        (MAINTAIN_WEIGHT, 'Maintain Weight'),
+        (GAIN_WEIGHT, 'Gain Weight'),
     )
-    goal = models.CharField(max_length=2,choices=GOALS,default=MAINTAIN_WEIGHT)
-    
+    goal = models.CharField(max_length=2, choices=GOALS, default=MAINTAIN_WEIGHT)
     MALE = 'M'
     FEMALE = 'F'
     GENDER = (
         (MALE, 'male'),
         (FEMALE, 'female'),
     )
-    gender = models.CharField(max_length=1,choices=GENDER,default=FEMALE)
-    
+    gender = models.CharField(max_length=1, choices=GENDER, default=FEMALE)
+
     date_of_birth = models.DateField()
 
     SEDENTARY = 'S'
@@ -35,13 +35,13 @@ class User(models.Model):
     ACTIVE = 'A'
     VERY_ACTIVE = 'VA'
     LEVELS = (
-        (SEDENTARY,'Sedentary'),
-        (LIGHTLY_ACTIVE,'Lightly Active'),
-        (ACTIVE,'Active'),
-        (VERY_ACTIVE,'Very Active'),
+        (SEDENTARY, 'Sedentary'),
+        (LIGHTLY_ACTIVE, 'Lightly Active'),
+        (ACTIVE, 'Active'),
+        (VERY_ACTIVE, 'Very Active'),
     )
-    activity_level = models.CharField(max_length=2,choices=LEVELS,default=LIGHTLY_ACTIVE)
-    
+    activity_level = models.CharField(max_length=2, choices=LEVELS, default=LIGHTLY_ACTIVE)
+
     workouts_per_week = models.PositiveSmallIntegerField()
     workout_minutes = models.PositiveSmallIntegerField()
     date_signed_up = models.DateTimeField(auto_now_add=True)
