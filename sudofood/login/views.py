6c242ccfc963c13d692dd3de9d9021598b3f0b18
danielleasavage <dsavage@dons.usfca.edu>
2018-04-12 16:42:09 -0700

    look at these views

--- sudofood/login/views.py
+++ sudofood/login/views.py
@@ -4,14 +4,14 @@ from django.template import loader
 
 from .models import User
 
+
 # Create your views here.
 def index(request):
     template = loader.get_template('login/index.html')
     context = {}
-    return render(request, 'login/index.html', context) 
+    return render(request, 'login/index.html', context)
     # return HttpResponse(template.render(context,request))
 
+
 def user(request, user_id):
-    return HttpResponse("This is the profile page for user %s!"%(user_id))
-
-
+    return HttpResponse("This is the profile page for user %s!" % (user_id))
