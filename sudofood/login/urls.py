42c383951e370ae4a4c397c56697ff3142ee2195
danielleasavage <dsavage@dons.usfca.edu>
2018-04-12 16:39:41 -0700

    ALSO PEP8

--- sudofood/login/urls.py
+++ sudofood/login/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('<int:user_id>/',views.user, name="profile"),
+    path('', views.index, name='index'),
+    path('<int:user_id>/', views.user, name="profile"),
 ]
