e9f3728ccffc31653d0bae4921b5df6efd4994e7
Daniel Grzenda <dcgrzenda@gmail.com>
2018-04-28 12:51:44 -0700

    working on init

--- application/__init__.py
+++ application/__init__.py
@@ -13,5 +13,18 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 
+with open('../MachineLearning/picklefiles/recipe_id.pkl','rb') as f:
+    recipe_id = pickle.load(f)
+
+with open('../MachineLearning/picklefiles/ingredients.pkl','rb') as f:
+    ingredients = pickle.load(f)
+
+with open('../MachineLearning/picklefiles/nutrients.pkl','rb') as f:
+    nutrients = pickle.load(f)
+
+with open('../MachineLearning/picklefiles/servings.pkl','rb') as f:
+    servings = pickle.load(f)
+
+
 # avoids the circular imports
 from application import routes, models
